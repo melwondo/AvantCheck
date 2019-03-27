4460f04c8cedb2a5a65817f66588e05816bd1afe
melwondo <42609874+melwondo@users.noreply.github.com>
2019-03-27 16:00:39 +0100

    Create fonctionIndi.php

---
+++ fonctionIndi.php
@@ -0,0 +1,6 @@
+<?php
+function writeSecretSentence(string $mystere1, string $mystere2){
+    return $mystere1. ' s\'incline devant ' .$mystere2;
+}
+echo writeSecretSentence('lune', 'poisson');
+?>
