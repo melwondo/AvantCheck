7958c2dd2c77a30b12bcd4f6ba33be47aff8595a
melwondo <42609874+melwondo@users.noreply.github.com>
2019-03-27 15:51:00 +0100

    Create Indiana.php

---
+++ Indiana.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nom = "Indiana Jones and the Last Crusade";
+    echo '<br>';
+    echo $nom;
+    echo '<br>';
+    $vu = "non";
+    
+    if ($vu == "non")
+    {
+        echo $vu;
+    }
+    echo '<br>';
+    $sortie = "18 October 1989";
+    echo $sortie;
+    echo '<br>';
+    $note = 8.3;
+    echo $note;
+    ?>
+    
+</body>
+</html>
